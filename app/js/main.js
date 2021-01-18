84bfcc7842ef40340cc81924bf7d965672ffedff
dtr200 <jaxer@list.ru>
2021-01-19 00:53:25 +0300

    readme + js comments  added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+// Классы галереи: основные изображения, кнопки вверх-вниз, точки и активная точка, основная галерея
+
 const galleryData = {
     'images': ['main-gallery--img', 'main-gallery--img2', 'main-gallery--img3', 'main-gallery--img4', 'main-gallery--img5'],
     'upButton': '.main-gallery__nav-button--up',
@@ -7,10 +9,12 @@ const galleryData = {
     'mainElement': '.main-gallery'
 }
 
+// Кнопка "Вверх": класс кнопки
 const buttonData = {
     'button': '.up-button'
 }
 
+// Классы для пагинации: все статьи (для opacity), для скрытия, активная кнопка, все кнопки 
 const paginationData = {
     'articles': '.article',
     'hide': 'pagination__hide',
